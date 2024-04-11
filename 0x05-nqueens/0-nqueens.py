dd5c762ffec321c2961a1c8f2fd87717fc19eb83
ett99it <gosudosu0@gmail.com>
2024-04-11 10:09:55 +0000

    0x05-nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -18,56 +18,56 @@ You are only allowed to import the sys module.
 import sys
 
 
-def n_queens(n):
-    """ N queens solution """
-    queens, res = [], []
-    cols, positive_diag, negative_diag = set(), set(), set()
+def is_safe(board, row, col, N):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 'Q':
+            return False
 
-    def backtrack(row, n, queens):
-        """ Backtracking function """
-        if row == n:
-            res.append(queens[:])
-            return
-        for col in range(n):
-            if (col in cols or row + col in positive_diag or
-                    row - col in negative_diag):
-                continue
-            cols.add(col)
-            positive_diag.add(row + col)
-            negative_diag.add(row - col)
-            queens.append([row, col])
-            backtrack(row + 1, n, queens)
+    # Check upper left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 'Q':
+            return False
 
-            cols.remove(col)
-            positive_diag.remove(row + col)
-            negative_diag.remove(row - col)
-            queens.pop()
-    backtrack(0, n, queens)
-    return res
+    # Check upper right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 'Q':
+            return False
+
+    return True
 
 
-def check_args(n):
-    """ Check if n is a valid argument """
-    if not n.isdigit():
+def solve(board, row, N):
+    if row == N:
+        for r in board:
+            print(' '.join(r))
+        print()
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row][col] = 'Q'
+            solve(board, row + 1, N)
+            board[row][col] = '.'
+
+
+def nqueens(N):
+    if not N.isdigit():
         print("N must be a number")
-        exit(1)
-    if int(n) < 4:
+        sys.exit(1)
+
+    N = int(N)
+    if N < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
 
-
-def main():
-    """ Main function """
-    args = sys.argv
-    if len(args) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    n = args[1]
-    check_args(n)
-    solutions = n_queens(int(n))
-    for solution in solutions:
-        print(solution)
+    board = [['.' for _ in range(N)] for _ in range(N)]
+    solve(board, 0, N)
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    nqueens(sys.argv[1])
